ca2dcb35040dd26db00d0b438afc4097e7cf481e
moacirsouza <25541880+moacirsouza@users.noreply.github.com>
2025-08-12 08:18:59 -0300

    Adds a simple palindrome checker

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,2 +1,10 @@
+ui=input("Type: ")
+
 def isPalindrome(s):
      return s.lower().replace(" ", "")==s.lower().replace(" ","")[::-1]
+
+a = isPalindrome(ui)
+if a:
+  print("Yes, it's a palindrom")
+else:
+  print("No, it's NOT a palindrom")
